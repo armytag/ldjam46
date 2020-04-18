f317c5fd93ffb6181b0bd378e1c6642dfc6c4eb4
Tristan A <armitageth@gmail.com>
2020-04-18 14:10:10 +0800

    Basic physics movement working

--- Player.gd
+++ Player.gd
@@ -1,10 +1,8 @@
-extends Light2D
+extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
 export (int) var SPEED
 var velocity = Vector2()
-var level = 1.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,23 +10,18 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	level -= 0.001
-	energy = level + rand_range(-0.02,0.02)
-#	print(energy)
-	
+func _physics_process(_delta):
 	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 	
-	position += velocity * delta
-	
+	var movement_vector = move_and_slide(velocity)
