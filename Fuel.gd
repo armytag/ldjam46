477be68de3235321ae508ebd5074a2f38e16fc82
Tristan A <armitageth@gmail.com>
2020-04-18 16:18:44 +0800

    Implemented fuel spawning

--- Fuel.gd
+++ Fuel.gd
@@ -8,3 +8,6 @@ func _ready():
 func _on_Fuel_body_entered(body):
 	emit_signal("collected")
 	queue_free()
+
+func _on_Fuel_collected():
+	print("Ooops!")
--- Main.gd
+++ Main.gd
@@ -1,11 +1,26 @@
 extends Node
 
-var fuel = 0;
+export (PackedScene) var Fuel
+var fuel_collected = 0;
 
 func _ready():
-	pass
+	randomize()
+	
+	var fuel_spawn_number = rand_range(100, 400)
+	for _i in range(fuel_spawn_number):
+		var fuel = Fuel.instance()
+		add_child(fuel)
+		fuel.position = Vector2(rand_range(-2000, 2000), rand_range(-2000,2000))
+		fuel.connect("collected", self, "_on_Fuel_collected")
 
 func _on_Fuel_collected():
-	fuel += 1
-	$HUD/FuelCount.text = "Fuel: " + String(fuel)
+	fuel_collected += 1
+	update_fuelcount(fuel_collected)
+
+func _on_Fuel_consumed():
+	fuel_collected -= 1
+	update_fuelcount(fuel_collected)
+	
+func update_fuelcount(fuel_amount):
+	$HUD/FuelCount.text = "Fuel: " + String(fuel_amount)
 	print($HUD/FuelCount.text)
