c563e9736f15f0790613dbb4563750135c8e9fb2
Tristan A <armitageth@gmail.com>
2020-04-20 01:08:38 +0800

    All sound effects added, just need to finish character animation

--- Main.gd
+++ Main.gd
@@ -39,11 +39,13 @@ func _process(_delta):
 			fuel_collected -= 1
 			var torch = Torch.instance()
 			add_child(torch)
+			$Player/PlaceTorchSound.play()
 			torch.position = $Player.position
 		if Input.is_action_just_pressed("ui_focus_prev") and fuel_collected > 0:
 			print("Reset player torch")
 			fuel_collected -= 1
 			torch_time_left = MAX_TORCH_TIME
+			$Player/ReplenishSound.play()
 		update_fuelcount(fuel_collected)
 	elif ready_to_restart:
 		if Input.is_action_just_pressed("ui_accept"):
@@ -55,10 +57,12 @@ func _process(_delta):
 func _on_Fuel_collected():
 	fuel_collected += 1
 	update_fuelcount(fuel_collected)
+	$Player/FuelCollectSound.play()
 
 func _on_Fuel_consumed():
 	fuel_collected -= 1
 	update_fuelcount(fuel_collected)
+#	$Player/ReplenishSound.play()
 	
 func update_fuelcount(fuel_amount):
 	$HUD/FuelCount.text = "Fuel Collected: " + String(fuel_amount)
@@ -104,5 +108,19 @@ func _on_TorchTimer_timeout():
 	var scale_factor = torch_time_left/MAX_TORCH_TIME
 	$Player/TorchLight.texture_scale = scale_factor
 	print(scale_factor)
-	
-	
+
+func _on_WindTimer_timeout():
+	$Player/WindSound.play()
+	$WindTimer.wait_time = rand_range(20,45)
+	$WindTimer.start()
+
+
+func _on_TempleArea_body_entered(body):
+	if body is KinematicBody2D:
+		$WindTimer.stop()
+#		$Player/WindSound.stop()
+
+
+func _on_TempleArea_body_exited(body):
+	if body is KinematicBody2D:
+		$WindTimer.start()
